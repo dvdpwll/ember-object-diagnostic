a782d8ae6f2d71d71a0694727ea550a784750a17
dvdpwll <dvdpwll456@gmail.com>
2016-09-19 09:25:53 -0400

    David Powell, Diagnostic

--- public/diagnostic.js
+++ public/diagnostic.js
@@ -1,4 +1,6 @@
 /// Ember Object Diagnostic ///
+import Ember from 'ember';
+
 
 // Use Ember Objects and Classes to represent a shopping cart!
 // Your abstractions will be `Cart` and `Order`.
@@ -7,6 +9,19 @@
 //  -  a unit price
 //  -  a quantity
 //  -  a computed property called `orderPrice`, equal to price times quantity
+
+const Order = Ember.Object.extend({
+  orderPrice: Ember.computed('unitPrice', 'quantity', function(){
+    return this.get('unitPrice') * this.get('quantity');
+  }),
+});
+
+let order = Order.create({
+  unitPrice: 5,
+  quantity: 3,
+});
+
+
 //
 // A Cart should have
 //  -  an `addToCart` method, which adds a given Item to an array
@@ -16,6 +31,23 @@
 //      the `orderPrice` values for everything in the cart); it should be
 //      recalculated any time an Order is added to the cart, removed from the
 //      cart, or modified.
+
+
+
+const Cart = Ember.Object.extend({
+  addToCart: function(){
+    //bloops
+      return ;
+  },
+  orders: [],
+  totalPrice: Ember.computed('orders.@each.orderPrice', function(){
+    let orders = this.get('orders');
+    //sum?
+    let total = orders.reduce((order) => order.orderPrice);
+    return total;
+  })
+});
+
 //
 // Once you've created the necessary Ember Classes, create a new Cart instance,
 //  and fill that cart up with three new product orders having the following
